30f8d3c6de0cc1ee7c2a5887e5b9fc3a4bc0d5c2
getDub <143089345+getDub@users.noreply.github.com>
2025-08-12 19:46:26 +1000

    Guess method now lets you know if you have guessed a letter before.

--- hm_game.rb
+++ hm_game.rb
@@ -20,10 +20,10 @@ class Game
     puts "However, if it is incorrect it will be counted as an attempt.\n\n"
     puts "Take your first guess\n"
     display_word(@secret_word = valid_word_size.sample(1).first.upcase)
+    # @secret_word = valid_word_size.sample(1).first.upcase
     p @secret_word
     while @attempts > 0 
-      @attempts -= 1
-      game_display(guess)
+      game_info(guess)
       # all_letters_revealed
     end
     puts "\nYour out of turns. You loose." if @attempts == 0
@@ -41,19 +41,18 @@ class Game
     end
   end
 
-  def game_display(letter)
-    puts @attempts > 0 ? "\nGuesses remaining = #{@attempts}" : "Guesses remaining = 0"
-    # puts remaining_guesses
-    # puts "\nGuesses remaining = #{@attempts}"
-    puts "Incorrect letters = #{@incorrect.join(", ")}"
-    puts "Correct letters = #{@correct.join(", ")}"
+  def game_info(player_input)
     puts "\n"
     puts secret_word.chars.map {|character| correct.include?(character) ? character : "_"}.join(" ")
+    puts @attempts > 0 ? "Guesses remaining = #{@attempts}" : "Guesses remaining = 0"
+    puts "Incorrect letters = #{@incorrect.join(", ")}"
+    puts "Correct letters = #{@correct.join(", ")}"
+    # puts "Correct letters = #{@correct}"
+    puts "\n"
   end
 
   def display_word(word)
-    no_of_spaces = word.size
-    no_of_spaces.times do
+    word.length.times do
       print " _"
     end
     print "\n\n"
@@ -62,6 +61,7 @@ class Game
   def guess
     guess = gets.chomp.upcase
     # winning_guess?(guess) if guess.length > 1 
+    puts "you've guessed this already" if guessed_already(guess)
     if guess.length > 1
       winning_guess?(guess)
     elsif
@@ -70,16 +70,22 @@ class Game
     # if @secret_word.include?(guess)
       # @correct << guess 
     else
+      @attempts -= 1 
       @incorrect << guess
     end
+  end
+
+  def guessed_already(players_guess)
+    @correct.include?(players_guess) || @incorrect.include?(players_guess)
   end
 
    def winning_guess?(word)
     if word.eql?(@secret_word)
       @attempts = -1
       puts "\nYOU WIN"
-      @correct = word#.split("")
-      puts "correct is#{@correct}"
+      @correct = word.split("")
+      # @correct << word
+      # puts "correct is#{@correct}"
     # elsif word.eql?(true)
     #   @attempts = -1
     #   puts "Solved! The secrect word is #{@secret_word}"
