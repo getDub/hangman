3b60269214756baeb3c983388a2a1db7d4f8b3c4
getDub <143089345+getDub@users.noreply.github.com>
2025-09-06 15:22:08 +1000

    Load_screen is now the games splash screen. Methods outside of Game class minimised to utilise class methods. Start_game internal code reduced to handle more mrthods from outside. Load_game works properly and calls start_game which delivers a where you left off intro which is different to new game intro.

--- hm_game.rb
+++ hm_game.rb
@@ -2,31 +2,27 @@ require "yaml"
 
 class Game
 
-  attr_accessor :dictionary, :secret_word, :attempts_left, :correct, :incorrect
-  
+  attr_accessor :dictionary, :secret_word, :attempts_left, :all_guesses, :correct, :incorrect
+
   def initialize
     @dictionary = dictionary
     @secret_word = secret_word
     @attempts_left = 7
+    @all_guesses = []
     @correct = []
     @incorrect = []
   end
 
   def start_game
     load_dictionary
-    puts "Hi Player, welcome to Hangman."
-    puts "Try and guess the secret word below one letter at a time."
-    puts "If you think you know the word you can type it in."
-    puts "However, if it is incorrect it will be counted as an attempt.\n\n"
-    puts "Take your first guess\n"
-    display_word(@secret_word = valid_word_size.sample(1).first.upcase)
+    intro if correct.empty? && incorrect.empty?
     p @secret_word
-    # while @attempts_left > 0 
-    #   game_info(guess)
-    #   all_letters_revealed ? winner : press1
-    #   puts "__________________________________"
-    # end
-    # puts "\nYour out of turns. You loose." if @attempts_left == 0
+    puts "Please see above game info of where you left off", game_info(@all_guesses.last) unless @all_guesses.empty?
+    while @attempts_left > 0 
+      game_info(guess)
+      all_letters_revealed ? winner : save_this_game?
+    end
+    puts "\nYour out of turns. You loose." if @attempts_left == 0
   end
 
   def load_dictionary
@@ -40,34 +36,40 @@ class Game
     end
   end
 
+  def choose_secret_word
+    @secret_word = valid_word_size.sample(1).first.upcase
+  end
+  
+  def intro
+    puts "Hi Player, welcome to Hangman."
+    puts "Try and guess the secret word below one letter at a time."
+    puts "If you think you know the word you can type it in."
+    puts "However, if it is incorrect it will be counted as an attempt.\n\n"
+    print "Take your first guess\n"
+    choose_secret_word
+    display_word
+  end
+
   def game_info(player_input)
     puts "\n"
-    puts secret_word.chars.map {|character| correct.include?(character) ? character : "_"}.join(" ")
+    display_word
     puts @attempts_left > 0 ? "Guesses remaining = #{@attempts_left}" : "Guesses remaining = 0"
     puts "Incorrect letters = #{@incorrect.join(", ")}"
     puts "Correct letters = #{@correct.join(", ")}"
     puts "\n"
   end
 
-  def display_word(word)
-    word.length.times do
-      print " _"
-    end
-    print "\n\n"
+  def display_word
+    print "secret word: ", secret_word.chars.map {|character| correct.include?(character) ? character : "_"}.join(" ")
+    print"\n\n"
   end
 
   def guess
     guess = gets.chomp.upcase
+    @all_guesses << guess
 
-    if guess == "1"
-      puts "Save as?"
-      file_name = gets.chomp
-      save_game(file_name)
-    # elsif guess == "2"
-      # load_game(saved_games)
-      
-    end
-    
+    save_as? if guess == "1"
+
     puts "you've guessed this already" if guessed_already(guess)
 
     if guess.length > 1
@@ -101,13 +103,23 @@ class Game
      @secret_word.chars.all? {|character| @correct.include?(character)}
   end
 
+  def spacer
+      puts "__________________________________"
+  end
+
   def winner
     @attempts_left = -1
     puts "Congrats...YOU win!"
   end
 
-  def press1
+  def save_this_game?
     puts "Press 1 to Save Game"
+  end
+
+  def save_as?
+    puts "Save as? Then press enter to save."
+      file_name = gets.chomp
+      save_game(file_name)
   end
 
   def save_game(file_name)
@@ -126,48 +138,39 @@ class Game
 
 end
 
-# hm = Game.new
-# hm.start_game
-
-
 def load_screen
   puts "Press [n] to start a new game"
   puts "Press [l] to load existing game"
   choice = gets.chomp.downcase
   if choice == "n"
-    print "\n___________________________________________________________\n"
+    spacer
     start_new_game
   elsif choice == "l"
-    play(Game.load_game(saved_games))
+    spacer
+    where_you_left_off
   end
 end
 
+def spacer
+  print "\n___________________________________________________________\n"  
+end
 
 def start_new_game
   Game.new().start_game
 end  
+
+def where_you_left_off
+  Game.load_game(saved_games).start_game
+end
   
 def saved_games
-  games = Dir.children("saves").each_with_index {|f, i| puts "#{i + 1}. #{f}"}
-  puts "type the number of the game file you would like to load."
-  file_number = gets.chomp.to_i
-  puts games[file_number - 1] 
-  games[file_number - 1] 
-end
-
-def play(game)
-  puts "This is the new game and #{game.secret_word} is the secret word."
-  puts "attempts left = #{game.attempts_left}, correct = #{game.correct} incorrect = #{game.incorrect}"
-  # game.display(game.secret_word)
-  # puts game.secret_word.class
-  # puts "Please guess a letter"
-  while game.attempts_left > 0 
-    game.game_info(game.guess)
-    game.all_letters_revealed ? game.winner : game.press1
-    puts "__________________________________"
-  end
-  puts "\nYour out of turns. You loose." if game.attempts_left == 0
-
+  puts "Saved games:"
+  games = Dir.children("saves").each_with_index {|f, i| puts "    #{i + 1}. #{f.gsub(".yaml", "")}"}
+  puts "Type the number of the game file you would like to load."
+  file_number = gets.chomp.to_i - 1
+  puts "    saved game '#{games[file_number].gsub(".yaml", "")}' loading...." 
+  spacer
+  games[file_number] 
 end
 
 load_screen
