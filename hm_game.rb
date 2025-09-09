7c0481371b8b4c7414bf7c9965c6ce656024f87b
getDub <143089345+getDub@users.noreply.github.com>
2025-09-09 19:26:03 +1000

    Process_answer handles fate of the players guess. Guess method now prompt_players_guess and shorter. Load_screen shortened as well by adding to mehtods that handle erroes and choices.

--- hm_game.rb
+++ hm_game.rb
@@ -1,11 +1,12 @@
-require "yaml"
+# frozen_string_literal: true
 
+require 'yaml'
+#hangman game
 class Game
-
   attr_accessor :dictionary, :secret_word, :attempts_left, :all_guesses, :correct, :incorrect
 
   def initialize
-    @dictionary = dictionary
+    @dictionary = load_dictionary
     @secret_word = secret_word
     @attempts_left = 7
     @all_guesses = []
@@ -14,16 +15,15 @@ class Game
   end
 
   def start_game
-    load_dictionary
     intro if correct.empty? && incorrect.empty?
     # p @secret_word #enable when testing
-    puts "Please see above game info of where you left off", game_info(@all_guesses.last) unless @all_guesses.empty?
-    while @attempts_left > 0 
-      game_info(guess)
+    puts 'Please see above game info of where you left off', game_info(@all_guesses.last) unless @all_guesses.empty?
+    while @attempts_left.positive?
+      prompt_players_guess
+      game_info
       all_letters_revealed ? winner : save_this_game?
     end
-    puts "\nYour out of turns. You loose." if @attempts_left == 0
-    puts "The secrect word was #{@secret_word}"
+    turn_tracker
   end
 
   def load_dictionary
@@ -33,57 +33,60 @@ class Game
 
   def valid_word_size
     dictionary.select do |word|
-    word.size >= 5 && word.size <= 12
+      word.size >= 5 && word.size <= 12
     end
   end
 
   def choose_secret_word
     @secret_word = valid_word_size.sample(1).first.upcase
   end
-  
+
+  def turn_tracker
+    puts 'Your out of turns. You loose.' if @attempts_left.zero?
+    puts "The secrect word was #{@secret_word}"
+  end
+
   def intro
-    puts "Hi Player, welcome to Hangman."
-    puts "Try and guess the secret word below one letter at a time."
-    puts "If you think you know the word you can type it in."
-    puts "However, if it is incorrect it will be counted as an attempt.\n\n"
+    print "Welcome to Hangman.\nTry and guess the secret word below one letter at a time.\n"
+    print "If you think you know the word you can type it in.\nHowever, if it is incorrect it will be counted as an attempt.\n\n"
     print "Take your first guess\n\n"
     choose_secret_word
     display_word
     spacer
   end
 
-  def game_info(player_input)
+  def game_info
     puts "\n"
     display_word
-    puts @attempts_left > 0 ? "Guesses remaining = #{@attempts_left}" : "Guesses remaining = 0"
-    puts "Incorrect letters = #{@incorrect.join(", ")}"
-    puts "Correct letters = #{@correct.join(", ")}"
-    puts "\n"
+    puts @attempts_left.positive? ? "Guesses remaining = #{@attempts_left}" : 'Guesses remaining = 0'
+    puts "Incorrect letters = #{@incorrect.join(', ')}\nCorrect letters = #{@correct.join(', ')}\n"
   end
 
   def display_word
-    print "secret word: ", secret_word.chars.map {|character| correct.include?(character) ? character : "_"}.join(" ")
-    print"\n\n"
+    print 'secret word: ', secret_word.chars.map { |character| correct.include?(character) ? character : '_' }.join(' ')
+    print "\n\n"
   end
 
-  def guess
+  def prompt_players_guess
     guess = gets.chomp.upcase
     @all_guesses << guess
+    process_answer(guess)
+  end
 
-    puts "    Your guess needs to be a letter not a number." if guess.to_i > 1
-    # save_as? if guess == "1"
-
-    puts "    You've guessed this already" if guessed_already(guess)
-
+  def process_answer(guess)
     if guess.length > 1
       word_guess(guess)
+    elsif guess.to_i > 1
+      puts '    Your guess needs to be a letter not a number.'
+    elsif guessed_already(guess)
+      puts "    You've guessed this already"
     elsif @secret_word.include?(guess)
       puts "    #{guess} is correct"
       @correct << guess
-    elsif guess == "1"
+    elsif guess == '1'
       save_as?
     else
-      @attempts_left -= 1 
+      @attempts_left -= 1
       puts "    #{guess} is incorrect"
       @incorrect << guess
     end
@@ -95,7 +98,7 @@ class Game
 
   def word_guess(word)
     if word.eql?(@secret_word)
-      @correct = word.split("")
+      @correct = word.split('')
     else
       puts "    \n #{word} is the incorrect word unfortunately"
       @attempts_left -= 1
@@ -104,92 +107,97 @@ class Game
   end
 
   def all_letters_revealed
-     @secret_word.chars.all? {|character| @correct.include?(character)}
+    @secret_word.chars.all? { |character| @correct.include?(character) }
   end
 
   def spacer
-      puts "__________________________________"
+    puts '__________________________________'
   end
 
   def winner
     @attempts_left = -1
-    puts "Congrats...YOU win!"
+    puts 'Congrats...YOU win!'
   end
 
   def save_this_game?
-    puts "Press 1 to Save Game"
+    puts 'Press 1 to Save Game'
     spacer
   end
 
   def save_as?
-    puts "Save as? Then press enter to save."
-      file_name = gets.chomp
-      save_game(file_name)
+    puts 'Save as? Then press enter to save.'
+    file_name = gets.chomp
+    save_game(file_name)
   end
 
   def save_game(file_name)
-    Dir.mkdir("saves") unless Dir.exist?("saves")
-    puts "file already exists" if File.exist?('file_name')
-    current_game_data = YAML::dump(self)
-    File.open("saves/#{file_name}.yaml", "w") {|f| f.write(current_game_data)}
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+    puts 'file already exists' if File.exist?('file_name')
+    current_game_data = YAML.dump(self)
+    File.open("saves/#{file_name}.yaml", 'w') { |f| f.write(current_game_data) }
     puts "Thank you. '#{file_name}' has been saved"
   end
- 
+
   def self.load_game(saved_games_file)
     game_file = File.open("saves/#{saved_games_file}")
     yaml = game_file.read
-    YAML::load(yaml, permitted_classes: [Game])
+    YAML.safe_load(yaml, permitted_classes: [Game])
   end
-
 end
 
 def load_screen
   print "------ HANGMAN ------\n"
-  puts "Press [n] to start a new game"
-  puts "Press [l] to load existing game"
-  
+  puts 'Press [n] to start a new game'
+  puts 'Press [l] to load existing game'
   choice = gets.chomp.downcase
-  while choice.match(/[^nl]/)
-    puts "na...select again."
+  switch_to_selection(choice)
+end
+
+def error_handling(player_input)
+  while player_input.match(/[^nl]/)
+    puts 'Your selction must be either [n] or [l].'
     choice = gets.chomp.downcase
   end
   choice
+end
 
-  if choice.match("n")
+def switch_to_selection(selection)
+  case selection
+  when 'n'
     spacer
     start_new_game
-  elsif choice.match("l")
+  when 'l'
     spacer
     where_you_left_off
   else
-    puts "    Your selection needs to be either l or n. Try again." 
+    error_handling(selection)
   end
 end
 
 def spacer
-  print "\n___________________________________________________________\n"  
+  print "\n___________________________________________________________\n"
 end
 
 def start_new_game
-  Game.new().start_game
-end  
+  Game.new.start_game
+end
 
 def where_you_left_off
   Game.load_game(saved_games).start_game
 end
-  
+
 def saved_games
-  puts "Saved games:"
-  games = Dir.children("saves").each_with_index {|f, i| puts "    #{i + 1}. #{f.gsub(".yaml", "")}"}
-  puts "Type the number of the game file you would like to load."
+  puts 'Saved games:'
+  games = Dir.children('saves').each_with_index { |f, i| puts "    #{i + 1}. #{f.gsub('.yaml', '')}" }
+  puts 'Type the number of the game file you would like to load.'
   file_number = gets.chomp.to_i - 1
-  while file_number > games.length || file_number.between?(-1,0)
-    puts "Incorrect number. Please select from the numbers displayed to the left of the file"
-    file_number = gets.chomp.to_i
+  while file_number > games.length || file_number.between?(-1, 0)
+    puts 'Incorrect number. Please select from the numbers displayed to the left of the file'
+    file_number = gets.chomp.to_i - 1
   end
-  puts "    saved game '#{games[file_number].gsub(".yaml", "")}' loading...." 
+  puts "    saved game '#{games[file_number].gsub('.yaml', '')}' loading...."
   spacer
-  games[file_number] 
+  games[file_number]
 end
 
 load_screen
